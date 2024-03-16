abf5fd8a910a8f86ff5882f7d2dc70afba311a9c
Merit Mohammed <cussmeritmohammed@gmail.com>
2024-03-16 08:47:28 +0100

    Third Commit

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -4,6 +4,18 @@ import { authConfig } from "../backend/firebase.config";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 
+//importing css code
+import ".././assets/css/style.css";
+
+//importing css vendor variables
+import ".././assets/vendor/bootstrap/css/bootstrap.min.css";
+import ".././assets/vendor/bootstrap-icons/bootstrap-icons.css";
+import ".././assets/vendor/boxicons/css/boxicons.min.css";
+import ".././assets/vendor/glightbox/css/glightbox.min.css";
+import ".././assets/vendor/remixicon/remixicon.css";
+import ".././assets/vendor/swiper/swiper-bundle.min.css";
+
+
 function LoginScreen() {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
@@ -21,7 +33,7 @@ function LoginScreen() {
       const user = userCredential.user;
       console.log("Logging user ", user.uid);
 
-      // Show success alert
+      // Show success alertss
       Swal.fire({
         icon: "success",
         title: "Login successful!",
@@ -60,18 +72,8 @@ function LoginScreen() {
                       href="index.html"
                       className="logo d-flex align-items-center w-auto"
                     >
-                      <span className="d-none d-lg-block">
-                        {" "}
-                        <span
-                          style={{
-                            color: "green",
-                          }}
-                        >
-                          {" "}
-                          AVINX{" "}
-                        </span>{" "}
-                        HEALTH{" "}
-                      </span>
+                    
+                       
                     </button>
                   </div>
                   <form action="" className="mt" method="post">
@@ -79,8 +81,7 @@ function LoginScreen() {
                       <div className="card-body">
                         <div className="pt-4 pb-2">
                           <h5 className="card-title text-center pb-0 fs-4">
-                            {" "}
-                            AVA is here to help you{" "}
+                            Staff Entry
                           </h5>
                         </div>
 
