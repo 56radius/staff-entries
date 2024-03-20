7a288a276e6b5f92ad1627a5253835d20e9f2d25
Merit Mohammed <cussmeritmohammed@gmail.com>
2024-03-20 14:24:24 +0100

    Changing the top category

--- src/components/Dashboard/HelpSupportScreen.js
+++ src/components/Dashboard/HelpSupportScreen.js
@@ -110,8 +110,8 @@ function HelpSupportScreen() {
                                
                                 <nav>
                                     <ol className="breadcrumb">
-                                        <li className="breadcrumb-item"><a href="index.html">Home</a></li>
-                                        <li className="breadcrumb-item active">Dashboard</li>
+                                        <li className="breadcrumb-item"><a href="index.html"> Dashboard </a></li>
+                                        <li className="breadcrumb-item active"> Help </li>
                                     </ol>
                                 </nav>
                             </div>
